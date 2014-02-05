149a88c11766361325616bd5da38bef4509affca
OrganicCat <lee.delarm@gmail.com>
2014-02-04 22:17:09 -0600

    Make a server
    Now we have a working server!

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+// Requres http server module
+var http = require("http");
+
+// Sets up server with request/response callback function that responds with a 
+// positive (200) plain-text response with "Hello World" and then sends the end
+// response to the browser
+http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
