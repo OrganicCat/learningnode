e1bec431cd4999386a17ce0469d22ed40ccb3b46
OrganicCat <lee.delarm@gmail.com>
2014-02-05 20:58:03 -0600

    Adding router
    All those requests? Let's make them birds now. Yeah, they're birds.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // Requres http server module
 var http = require("http");
+// Whoa shizzle, a new module that can parse the url of the request?
+// What sorcery is this?
+var url = require("url");
 
-function start() {
+// We are now passing the route function from the router module here
+function start(route) {
 	function onRequest(request, response) {
+
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received");
+
+		// About to get our routing on
+		route(pathname);
+
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();
