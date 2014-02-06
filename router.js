a5a48fac5fab8f94ad412ae24851e15094ad1a68
OrganicCat <lee.delarm@gmail.com>
2014-02-05 21:30:18 -0600

    Added request handlers
    I'm not sure this feels like the right way to add request handlers.
    Shouldn't it be more dynamic with the ability to just call any handler
    you want without predefining them in the index.js file?

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
-function route(pathname) {
+function route(handle, pathname, response) {
 	console.log("Routing a request for " + pathname);
+	if(typeof handle[pathname] === 'function') {
+		handle[pathname](response);
+	} else {
+		console.log("No request handler found for " + pathname);
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("Cats have knocked this page off the web and hidden it under your couch");
+		response.end();
+	}
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -5,18 +5,14 @@ var http = require("http");
 var url = require("url");
 
 // We are now passing the route function from the router module here
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received");
 
-		// About to get our routing on
-		route(pathname);
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
+		// Let's let our handlers deal with the proper response
+		route(handle, pathname, response);
 	}
 
 	// Pass in our request/response function
