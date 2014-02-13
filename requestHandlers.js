968554d58ec4e7d431bd595be050e3b3d399764a
OrganicCat <lee.delarm@gmail.com>
2014-02-12 20:41:21 -0600

    New request handler, better image paths
    Created new image handler with new jade layout to make sure I understand
    the basics. Also moved images to proper folder. Removed excess old
    handlers and code

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,62 +2,14 @@ var querystring = require("querystring"),
 	fs = require("fs"),
 	formidable = require("formidable");
 
-function start2(request, response) {
+function start(request, response) {
 	response.render('index', { title: 'Express' });
 }
 
-function start(response, request) {
-	console.log("Request handler 'start' was called");
+function home(request, response) {
+	response.render('home', { title: 'Cats in this bag'});
 
-	var body = '<html>'+
-		'<head>'+
-		'<meta http-equiv="Content-Type" content="text/html; '+
-		'charset=UTF-8" />'+
-		'</head>'+
-		'<body>'+
-		'<form action="/upload" enctype="multipart/form-data" method="post">'+
-		'<input type="file" name="upload">'+
-		'<input type="submit" value="Upload file" />'+
-		'</form>'+
-		'</body>'+
-		'</html>';
-
-	
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write(body);
-	response.end();
-
-}
-
-function upload(response, request) {
-	console.log("Request handler 'upload' was called");
-
-	// Create form object using formidable
-	var form = new formidable.IncomingForm();
-	form.parse(request, function(error, fields, files) {
-		// workaround for windows, deletes file before replacing
-		fs.rename(files.upload.path, "test/cats.jpg", function(error) {
-			if(error) {
-				fs.unlink("test/cats.jpg");
-				fs.rename(files.upload.path, "test/cats.jpg");
-			}
-		});
-	});
-
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("received image: <br/>");
-	response.write("<img src='/show' />")
-	response.end();
-	
-}
-
-function show(response) {
-	console.log("Request handler 'show' was called");
-	response.writeHead(200, {"Content-Type": "image/jpeg"});
-	fs.createReadStream("test/cats.jpg").pipe(response);
 }
 
 exports.start = start;
-exports.start2 = start2;
-exports.upload = upload;
-exports.show = show;
+exports.home = home;
--- routes/router.js
+++ routes/router.js
@@ -3,21 +3,9 @@ function setup(app, requestHandlers) {
 
 	// Base test call, move into request handler as soon as possible
 	// * pass the request, get the handler from that
-	app.get('/', requestHandlers.start2);
+	app.get('/', requestHandlers.start);
+	app.get('/home', requestHandlers.home);
+
 }
 
 exports.setup = setup;
-
-/*
-function route(handle, pathname, response, request) {
-	console.log("Routing a request for " + pathname);
-	if(typeof handle[pathname] === 'function') {
-		handle[pathname](response, request);
-	} else {
-		console.log("No request handler found for " + pathname);
-		response.writeHead(404, {"Content-Type": "text/plain"});
-		response.write("Cats have knocked this page off the web and hidden it under your couch");
-		response.end();
-	}
-}
-*/
